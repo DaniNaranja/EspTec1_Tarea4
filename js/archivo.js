f75dc568594d2dce4f4d1f0873d99b538984ce1d
Daniela <d.paredes04@ufromail.cl>
2022-10-08 11:01:22 -0300

    Se añade formulario y funciones en botones

---
+++ js/archivo.js
@@ -0,0 +1,17 @@
+
+function after1(){
+    document.getElementById('image1')
+    .src='https://www.boredpanda.com/blog/wp-content/uploads/2020/07/shiba-cheems-meme-dog-balltze-5-5f030ec3cf823__700.jpg';
+}
+
+function after2(){
+    document.getElementById('parrafo2')
+    .innerHTML="Este es un parrafo modificado."
+}
+
+function after3(){
+    const tituloAves = document.getElementById('aves-titulo');
+    tituloAves.style.backgroundColor = '#B2DAFA';  
+    tituloAves.style.fontSize= "3rem"            
+}
+
