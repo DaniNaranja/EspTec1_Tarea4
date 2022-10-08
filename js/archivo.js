a8c343a1f22eb8a4322b0e69f7275f28e5aa1b9f
Daniela <d.paredes04@ufromail.cl>
2022-10-08 11:24:27 -0300

    Se agrega logging del formulario

--- js/archivo.js
+++ js/archivo.js
@@ -1,4 +1,3 @@
-
 function after1(){
     document.getElementById('image1')
     .src='https://www.boredpanda.com/blog/wp-content/uploads/2020/07/shiba-cheems-meme-dog-balltze-5-5f030ec3cf823__700.jpg';
@@ -15,3 +14,16 @@ function after3(){
     tituloAves.style.fontSize= "3rem"            
 }
 
+function imprimir(){
+    var email= document.getElementById('email');
+    var name= document.getElementById('name');
+    var edad= document.getElementById('edad');
+    var ciudad= document.getElementById('ciudad');
+    var pregunta1= document.getElementById('pregunta1');
+
+    console.log(email.value);
+    console.log(name.value);
+    console.log(edad.value);
+    console.log(ciudad.value);
+    console.log(pregunta1.value);
+}
